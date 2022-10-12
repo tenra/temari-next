c92d06c9a149f1c7c4465114e3427df53fa8b697
tenra <tetenchi@yahoo.co.jp>
2022-10-12 12:00:28 +0900

    buttton color

--- components/contactForm.js
+++ components/contactForm.js
@@ -128,7 +128,7 @@ function ContactForm() {
               disabled={status.submitting}
               className="relative px-6 py-3 font-bold text-black group text-xl"
             >
-              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0 rounded"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-sky-300 group-hover:translate-x-0 group-hover:translate-y-0 rounded"></span>
               <span className="absolute inset-0 w-full h-full border-2 border-black dark:border-gray-200 rounded"></span>
               <span className="relative">
                 <svg className="w-6 h-6 stroke-black relative bottom-0.5 inline-block mr-2" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#send-diagonal_svg__clip0_2476_13290)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><path d="M22.152 3.553L11.178 21.004l-1.67-8.596L2 7.898l20.152-4.345zM9.456 12.444l12.696-8.89"></path></g><defs><clipPath id="send-diagonal_svg__clip0_2476_13290"><path fill="#fff" d="M0 0h24v24H0z"></path></clipPath></defs></svg>
